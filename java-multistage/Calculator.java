43d6433cf83dedc386e20b2c3894d9b3c57a112a
suryaadev <rsurya1760@gmail.com>
2024-01-27 11:09:06 +0530

    multistage docker file

---
+++ java-multistage/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+    public static void main(String[] args) {
+            // Hardcoded numbers
+	            int num1 = 10;
+		            int num2 = 20;
+
+			            // Add numbers and print result
+				            int sum = num1 + num2;
+					            System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+						        }
+							}
+
---
+++ python-multistage/main.py
@@ -0,0 +1,1 @@
+print("this is python multi-stage application")
